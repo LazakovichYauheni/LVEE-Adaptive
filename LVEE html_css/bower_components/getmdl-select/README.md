d62c9f0d97de46613d9dabf61bad84b086c5dc63
Евгений Лазакович <zhenialazacovich@gmail.com>
2020-04-13 17:05:28 +0300

    Initial commit

---
+++ LVEE html_css/bower_components/getmdl-select/README.md
@@ -0,0 +1,82 @@
+    
+# getmdl-select
+Material Design Lite selectfield component [material-design-lite](https://github.com/google/material-design-lite)
+
+## Live Example
+
+![alt tag](https://raw.github.com/CreativeIT/getmdl-select/gh-pages/lib/index_mdl/select_mdl.gif)
+
+Check out the [example](http://creativeit.github.io/getmdl-select/)
+
+## Install
+
+There are three ways to install getmdl-select:
+
+ 1. Using [Bower](http://bower.io/):
+    Use this command in your command line: 
+    ```bash
+    bower install getmdl-select
+    ```
+ 2. Using [npm](http://npmjs.org/): 
+    Use this command in your command line: 
+    ```bash
+    npm install getmdl-select
+    ```
+ 3. Clone the repo using Git:
+
+    ```bash
+    git clone https://github.com/CreativeIT/getmdl-select.git
+    ```
+    
+    (Alternatively you can [download](https://github.com/CreativeIT/getmdl-select/archive/master.zip)
+this repository.)
+    And then:
+    
+    ```bash
+    cd getmdl-select   
+    npm install
+    ```
+
+## Basic use
+To use any MDL component, you must include the minified CSS and JavaScript files using standard relative-path references in the `<head>` section of the page, as described in the MDL Introduction.
+   ```html
+   <!-- getmdl -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+   <link rel="stylesheet" href="your_path_to/material-design-lite/material.min.css">
+   <script defer src="your_path_to/material-design-lite/material.min.js"></script>   
+   <!--getmdl-select-->   
+   <link rel="stylesheet" href=path_to/getmdl-select/getmdl-select.min.css">
+   <script defer src="path_to/getmdl-select/getmdl-select.min.js"></script>
+   ```
+
+### Example
+
+Select field.
+ ```html
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
+      <input class="mdl-textfield__input" id="country" name="country" value="Belarus" type="text" readonly tabIndex="-1" data-val="BLR"/>
+        <label class="mdl-textfield__label" for="country">Country</label>
+        <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu" for="country">
+          <li class="mdl-menu__item" data-val="BLR">Belarus</li>
+          <li class="mdl-menu__item" data-val="RUS">Russia</li>
+        </ul>
+    </div>
+ ```
+### Important
+If you want to use more than one getmdl-select item use different ids for inputs and don't forget to change `ul` and `label`'s `for` attributes. It works wrong without this.
+
+## Configuration options 
+#### Dynamically usage
+For dynamically usage, you must add `getmdlSelect.init(cssSelector)` in javascript code, (where cssSelector, for example, is `".getmdl-select"` or `"#mySelect"`), after new item is created or any new element added to existing list. 
+
+#### data-val
+Every `li` should have its own `data-val` attribute. Choosing any element of select you change `input`'s `data-val` property, which you can get using `document.querySelector(selector).getAttribute('data-val');`, where selector is your `input`'s id.
+
+#### Width
+Initial Select takes the default width (300px). If you want that Select automatically adapt to the maximum width add class `getmdl-select__fullwidth`.
+
+#### Height
+Select automatically adapt to the maximum height. If you want to use small height (300px) and see scroll bar, add class `getmdl-select__fix-height`.    
+    
+## LICENSE
+See the [LICENSE file](https://github.com/CreativeIT/getmdl-select/blob/master/LICENSE.txt) for license rights and limitations (MIT).
---
+++ LVEE html_css/code.js
@@ -0,0 +1,213 @@
+var isOpened = false;
+var isContactOpened = false;
+var isPartOpened = false;
+var isSponsOpened = false;
+
+$(window).scroll(function() 
+{
+    var scroll = $(window).scrollTop();
+
+    if (scroll <= 50) {
+        $(".menu").removeClass("transparent");
+    } else if (scroll >= 50) {
+        $(".menu").addClass("transparent");
+    } else {
+        $(".menu").removeClass("transparent");
+    }
+});
+
+function openMenu() 
+{
+    $('#menu').addClass('menucolor');
+}
+
+function openitemslist()
+{
+    
+    if(isOpened == false)
+    {
+        document.getElementById("itemslist").style.display = "flex";
+        isOpened = true;
+    }
+    else if (isOpened == true)
+    {
+        document.getElementById("itemslist").style.display = "none";
+        isOpened = false;
+    }
+
+}
+
+function opencontactitemslist()
+{
+
+    if(isContactOpened == false)
+    {
+        document.getElementById("itemscontactlist").style.display = "flex";
+        isContactOpened = true;
+    }
+    else if (isContactOpened == true)
+    {
+        document.getElementById("itemscontactlist").style.display = "none";
+        isContactOpened = false;
+    }
+
+}
+
+function openpartitemslist()
+{
+
+    if(isPartOpened == false)
+    {
+        document.getElementById("itemspartlist").style.display = "flex";
+        isPartOpened = true;
+    }
+    else if (isPartOpened == true)
+    {
+        document.getElementById("itemspartlist").style.display = "none";
+        isPartOpened = false;
+    }
+
+}
+
+function opensponsitemslist()
+{
+
+    if(isSponsOpened == false)
+    {
+        document.getElementById("itemssponslist").style.display = "flex";
+        isSponsOpened = true;
+    }
+    else if (isSponsOpened == true)
+    {
+        document.getElementById("itemssponslist").style.display = "none";
+        isSponsOpened = false;
+    }
+
+}
+
+function openNav() 
+{
+    
+    if(jQuery('body').width()<960)
+    {
+        document.getElementById("mySidenav").style.width = "100vw";
+    }
+    if(jQuery('body').width()>=960 && jQuery('body').width() <= 1121)
+    {
+        document.getElementById("mySidenav").style.width = "75vw";
+    }
+    if(jQuery('body').width()>1121)
+    {
+        document.getElementById("mySidenav").style.width = "60vw";
+    }
+}
+
+function closeNav() 
+{
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function dropdown() 
+{
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+function openNavRight() 
+{
+    $("#navRight").removeClass('animated fadeOutRight');
+    $("#navRight").addClass('animated fadeInRight').css('animation-duration','0.6s');;
+    if(jQuery('body').width()<960)
+    {
+        document.getElementById("navRight").style.width = "100vw";
+        document.getElementById("navRight").style.display = "block";
+    }
+
+    if(jQuery('body').width()<1146 && jQuery('body').width()>960)
+    {
+        document.getElementById("navRight").style.width = "30vw";
+        document.getElementById("navRight").style.display = "block";
+    }
+
+    if(jQuery('body').width()>1146)
+    {
+        document.getElementById("navRight").style.width = "20vw";
+        document.getElementById("navRight").style.display = "block";
+    }
+}
+
+function closeNavRight() 
+{
+    $("#navRight").addClass('animated fadeOutRight').css('animation-duration','0.6s');
+    //document.getElementById("navRight").style.display = "none";
+    $(".menu").removeClass("menucolor");
+}
+
+function closeScreens(ID)
+{
+    $("#"+ID).addClass('animated fadeOutLeft').css('animation-duration','0.6s');
+}
+
+function closeNavR()
+{
+    $("#navRight").addClass('animated fadeOutRight').css('animation-duration','0.6s');
+}
+
+function closeScr(ID)
+{
+    document.getElementById(ID).style.display = "none";
+    $("#"+ID).removeClass('animated fadeOutLeft');
+}
+
+function showcontent(Div) 
+{
+    var x = document.getElementById(Div);
+    if (x.style.display == "none") 
+    {
+        x.style.display = "block";
+        $("#"+Div).addClass('animated fadeInLeft').css('animation-duration','0.6s');;
+    } 
+    else
+    {
+        x.style.display = "none";
+        $("#"+Div).removeClass('animated fadeOutLeft');
+    }
+}
+
+$(document).ready(function()
+{
+    $('.carousel').carousel(
+    {
+        fullWidth:true,
+        indicators:true
+    }).height(500);
+    $('#left').click(function() 
+    {
+        $('.carousel').carousel('prev');
+    });
+    
+    $('#right').click(function() 
+    {
+        $('.carousel').carousel('next');
+    });
+    setInterval(function() 
+    {
+        $('.carousel').carousel('next');
+    }, 5000);
+});
+
+jQuery(document).ready(function()
+{
+    function classFunction()
+    {
+      if(jQuery('body').width()>740)
+      { 
+          jQuery('.cardcontainer1.wow.fadeInLeft').removeClass('fadeInLeft').addClass('fadeIn').attr('data-wow-duration','3s');
+          jQuery('.cardcontainer1.cardcontainer2.wow.fadeInRight').removeClass('fadeInRight').addClass('fadeIn').attr('data-wow-duration','3s');
+          jQuery('.cardcontainer1.cardcontainer3.wow.fadeInLeft').removeClass('fadeInLeft').addClass('fadeIn').attr('data-wow-duration','3s');
+          jQuery('.cardcontainer1.cardcontainer4.wow.fadeInRight').removeClass('fadeInRight').addClass('fadeIn').attr('data-wow-duration','3s');
+          jQuery('.cardcontainer1.cardcontainer5.wow.fadeInLeft').removeClass('fadeInLeft').addClass('fadeIn').attr('data-wow-duration','3s');
+      }
+    }
+    classFunction();
+    jQuery(window).resize(classFunction);
+})
